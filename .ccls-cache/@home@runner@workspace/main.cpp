878263c520a5baf01b29c92c8b4444ce5e30827b
corti009 <co28109@email.vccs.edu>
2025-10-26 06:39:48 +0000

    Added program_2 and information for it

--- .ccls-cache/@home@runner@workspace/main.cpp
+++ .ccls-cache/@home@runner@workspace/main.cpp
@@ -2,10 +2,12 @@
 using namespace std;
 int main()
 {
-  char first, middle, last;
-  first = 'C';
-  middle = 'J';
-  last = 'O';
-  cout << first << middle << last;
-  return 0;
+  string name, adress, phoneNumber;
+  name = "Carlan Johnathan Ortiz";
+  adress = "2776 Butternut Dr.";
+  phoneNumber = "305-788-9090";
+  cout << name << endl;
+  cout << adress << endl;
+  cout << phoneNumber << endl;
+return 0;
 }

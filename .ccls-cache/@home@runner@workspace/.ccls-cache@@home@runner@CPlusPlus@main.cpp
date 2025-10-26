117a805c1299b22475af5b08927f4a4853744ced
co28109 <47647552-co28109@users.noreply.replit.com>
2025-10-26 06:14:35 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@workspace/.ccls-cache@@home@runner@CPlusPlus@main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+  char Carlan, Johnathan, Ortiz;
+  first = 'C';
+  middle = 'J';
+  last = 'O';
+  cout << first << middle << last;
+  return 0;
+}
---
+++ .ccls-cache/@home@runner@workspace/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+  char first, middle, last;
+  first = 'C';
+  middle = 'J';
+  last = 'O';
+  cout << first << middle << last;
+  return 0;
+}
